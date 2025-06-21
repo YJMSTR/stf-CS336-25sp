35323297c9c2c74316da11a12b916a16baac7e21
zhangyangjie <zhangyj12024@shanghaitech.edu.cn>
2025-06-21 14:26:05 +0800

    update bbpe train

--- assignment1-basics/cs336_basics/bbpe_train.py
+++ assignment1-basics/cs336_basics/bbpe_train.py
@@ -6,71 +6,63 @@ from functools import partial
 from collections import Counter
 import time
 from tqdm import tqdm
+import heapq
 
 def find_chunk_boundaries(
     file: BinaryIO, 
     desired_num_chunks: int, 
     split_special_token: bytes
 ) -> list[int]:
-    """
-    Chunk the file into parts that can be counted independently.
-    May return fewer chunks if the boundaries end up overlapping.
-    """
     assert isinstance(split_special_token, bytes), (
         "Must represent special token as a bytestring"
     )
 
-    # Get total file size in bytes
     file.seek(0, os.SEEK_END)
     file_size = file.tell()
     file.seek(0)
 
     chunk_size = file_size // desired_num_chunks
 
-    # Initial guesses for chunk boundary locations, uniformly spaced
-    # Chunks start on previous index, don't include last index
     chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
     chunk_boundaries[-1] = file_size
 
-    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+    mini_chunk_size = 4096
 
     for bi in range(1, len(chunk_boundaries) - 1):
         initial_position = chunk_boundaries[bi]
-        file.seek(initial_position)  # Start at boundary guess
+        file.seek(initial_position)
         while True:
-            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+            mini_chunk = file.read(mini_chunk_size)
 
-            # If EOF, this boundary should be at the end of the file
             if mini_chunk == b"":
                 chunk_boundaries[bi] = file_size
                 break
 
-            # Find the special token in the mini chunk
             found_at = mini_chunk.find(split_special_token)
             if found_at != -1:
                 chunk_boundaries[bi] = initial_position + found_at
                 break
             initial_position += mini_chunk_size
 
-    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
-def pre_tokenize(chunk: str, special_tokens: list[str]) -> tuple[list[list[bytes]]]:
-    """Pre-tokenize a chunk of text.
+def pre_tokenize(chunk: str, special_tokens: list[str]) -> tuple[list[list[int]], dict[int, bytes]]:
+    vocab = {i: bytes([i]) for i in range(256)}
+    byte_to_token_id = {v: k for k, v in vocab.items()}
 
-    Args:
-        chunk (str): The text to tokenize.
-        special_tokens (list[str]): List of special tokens to split on.
-    returns:
-        tuple[list[list[bytes]], dict[tuple[bytes, ...], int]]: A tuple containing:
-            - list[list[bytes]]: a list of "words", where each "word" is a list of its constituent bytes.
-                                 Special tokens are represented as a list containing a single bytes object.
-    """
-    special_tokens = sorted(special_tokens, key=lambda x: len(x), reverse=True)
-    escaped_tokens = [re.escape(token) for token in special_tokens]
+    for token in special_tokens:
+        token_bytes = token.encode('utf-8')
+        if token_bytes not in byte_to_token_id:
+            token_id = len(vocab)
+            vocab[token_id] = token_bytes
+            byte_to_token_id[token_bytes] = token_id
+
+    special_tokens_set = {token.encode('utf-8') for token in special_tokens}
+
+    special_tokens_sorted = sorted([t.encode('utf-8') for t in special_tokens], key=len, reverse=True)
+    
+    escaped_tokens = [re.escape(token.decode('utf-8')) for token in special_tokens_sorted]
     delimiter_pattern = "|".join(escaped_tokens)
-    print("special_tokens: ", special_tokens)
-    print("delimiter_pattern:", delimiter_pattern)
 
     if delimiter_pattern:
         chunks = re.split(f"({delimiter_pattern})", chunk)
@@ -79,113 +71,116 @@ def pre_tokenize(chunk: str, special_tokens: list[str]) -> tuple[list[list[bytes
 
     PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
-    processed_tokens_as_list_of_byte_sequences = []
+    processed_tokens_as_list_of_ids = []
     for sub_chunk in chunks:
-        if sub_chunk in special_tokens:
-            processed_tokens_as_list_of_byte_sequences.append([sub_chunk.encode('utf-8')])
+        sub_chunk_bytes = sub_chunk.encode('utf-8')
+        if sub_chunk_bytes in special_tokens_set:
+            token_id = byte_to_token_id[sub_chunk_bytes]
+            processed_tokens_as_list_of_ids.append([token_id])
         else:
             words_from_pat = [match.group() for match in re.finditer(PAT, sub_chunk)]
             for word_str in words_from_pat:
-                byte_sequence = [bytes([b]) for b in word_str.encode('utf-8')]
-                processed_tokens_as_list_of_byte_sequences.append(byte_sequence)
+                byte_sequence = word_str.encode('utf-8')
+                id_sequence = [b for b in byte_sequence]
+                processed_tokens_as_list_of_ids.append(id_sequence)
 
-    return processed_tokens_as_list_of_byte_sequences
+    return processed_tokens_as_list_of_ids, vocab
 
-def cal_freq(all_tokens: list[list[bytes]], special_tokens: list[str]) -> tuple[Counter[tuple[bytes, bytes]], dict[tuple[bytes, bytes], list[tuple[int, int]]]]:
+def cal_freq(all_tokens: list[list[int]], special_token_ids: set[int]) -> tuple[Counter[tuple[int, int]], dict[tuple[int, int], set[int]]]:
     pair_freq = Counter()
     pair_pos = {}
-    special_tokens_set = set(special_tokens) 
 
-    for i, token_byte_sequence in enumerate(all_tokens): 
-        is_current_token_special = False
-        if len(token_byte_sequence) == 1: 
-            try:
-                decoded_token_str = token_byte_sequence[0].decode('utf-8')
-                if decoded_token_str in special_tokens_set:
-                    is_current_token_special = True
-            except UnicodeDecodeError: 
-                pass 
+    for i, token_id_sequence in enumerate(all_tokens):
+        if len(token_id_sequence) == 1 and token_id_sequence[0] in special_token_ids:
+            continue
 
-        if is_current_token_special:
-            continue 
-
-        
-        for j in range(len(token_byte_sequence) - 1):
-            pair = (token_byte_sequence[j], token_byte_sequence[j+1])
+        for j in range(len(token_id_sequence) - 1):
+            pair = (token_id_sequence[j], token_id_sequence[j+1])
             pair_freq[pair] += 1
             if pair not in pair_pos:
-                pair_pos[pair] = []
-            pair_pos[pair].append((i, j))
+                pair_pos[pair] = set()
+            
+            packed_pos = (i << 32) | j
+            pair_pos[pair].add(packed_pos)
+            
     return pair_freq, pair_pos
 
-def merge_pair(best_pair: tuple[bytes, bytes], all_tokens: list[list[bytes]], pair_freq: Counter[tuple[bytes, bytes]], pair_pos: dict[tuple[bytes, bytes], list[tuple[int, int]]]) :
-    merged_symbol = best_pair[0] + best_pair[1] # The actual merged byte(s) string
-    pair_freq.pop(best_pair)
-    active_word_pos = set()
-    
-    # Create progress bar for positions to merge
-    positions_to_merge = pair_pos[best_pair]
-    pbar = tqdm(
-        total=len(positions_to_merge),
-        desc=f"Merging {best_pair}",
-        leave=False  # Don't leave the progress bar after completion
-    )
-    
-    for i, j in positions_to_merge:
-        if j > 0:
-            # prev pair
-            prev_pair = (all_tokens[i][j-1], best_pair[0])
-            pair_freq[prev_pair] -= 1
-            if pair_freq[prev_pair] == 0:
-                del pair_freq[prev_pair]
-            pair_freq[(all_tokens[i][j-1], merged_symbol)] += 1
-            active_word_pos.add(i)
-        if j + 2 < len(all_tokens[i]):
-            # next_pair
-            next_pair = (best_pair[1], all_tokens[i][j+2])
-            pair_freq[next_pair] -= 1
-            if pair_freq[next_pair] == 0:
-                del pair_freq[next_pair]
-            pair_freq[(merged_symbol, all_tokens[i][j+2])] += 1
-            active_word_pos.add(i)
-        pbar.update(1)
-    
-    pbar.close()
+class HeapItem:
+    vocab: dict[int, bytes] = {}
 
-    # Create progress bar for updating tokens
-    pbar = tqdm(
-        total=len(active_word_pos),
-        desc="Updating tokens",
-        leave=False
-    )
-    
-    for i in active_word_pos:
-        for j in range(len(all_tokens[i])-1):
-            pair = (all_tokens[i][j], all_tokens[i][j+1])
-            if pair in pair_pos:
-                pair_pos[pair].remove((i, j))
-        new_tokens = []
+    def __init__(self, freq: int, pair: tuple[int, int]):
+        self.freq = freq
+        self.pair = pair
+
+    def __lt__(self, other: "HeapItem") -> bool:
+        if self.freq != other.freq:
+            return self.freq > other.freq
+        
+        p1_bytes = (HeapItem.vocab[self.pair[0]], HeapItem.vocab[self.pair[1]])
+        p2_bytes = (HeapItem.vocab[other.pair[0]], HeapItem.vocab[other.pair[1]])
+        return p1_bytes > p2_bytes
+
+    def __repr__(self):
+        return f"HeapItem(freq={self.freq}, pair={self.pair})"
+
+def merge_pair(
+    best_pair: tuple[int, int],
+    all_tokens: list[list[int]],
+    pair_freq: Counter[tuple[int, int]],
+    pair_pos: dict[tuple[int, int], set[int]],
+    pq: list,
+    merged_token_id: int,
+) -> None:
+    affected_pair_types = set()
+
+    indices_of_words_to_update = set(pos >> 32 for pos in pair_pos[best_pair])
+
+    for i in indices_of_words_to_update:
+        token_id_sequence = all_tokens[i]
+
+        for j in range(len(token_id_sequence) - 1):
+            pair = (token_id_sequence[j], token_id_sequence[j+1])
+            if pair in pair_freq:
+                pair_freq[pair] -= 1
+                packed_pos = (i << 32) | j
+                pair_pos[pair].remove(packed_pos)
+                if pair_freq[pair] == 0:
+                    del pair_freq[pair]
+                    del pair_pos[pair]
+                affected_pair_types.add(pair)
+
+        new_token_id_sequence = []
         j = 0
-        while j < len(all_tokens[i]):
-            if j < len(all_tokens[i])-1 and (all_tokens[i][j], all_tokens[i][j+1]) == best_pair:
-                new_tokens.append(merged_symbol)
+        while j < len(token_id_sequence):
+            if j < len(token_id_sequence) - 1 and (token_id_sequence[j], token_id_sequence[j+1]) == best_pair:
+                new_token_id_sequence.append(merged_token_id)
                 j += 2
             else:
-                new_tokens.append(all_tokens[i][j])
+                new_token_id_sequence.append(token_id_sequence[j])
                 j += 1
-        all_tokens[i] = new_tokens
-        for j in range(len(all_tokens[i])-1):
-            pair = (all_tokens[i][j], all_tokens[i][j+1])
+        
+        all_tokens[i] = new_token_id_sequence
+
+        for j in range(len(new_token_id_sequence) - 1):
+            pair = (new_token_id_sequence[j], new_token_id_sequence[j+1])
             if pair not in pair_pos:
-                pair_pos[pair] = []
-            pair_pos[pair].append((i, j))
-        pbar.update(1)
-    
-    pbar.close()
-    pair_pos.pop(best_pair)
-    return all_tokens
+                pair_pos[pair] = set()
+            
+            packed_pos = (i << 32) | j
+            pair_pos[pair].add(packed_pos)
+            pair_freq[pair] = pair_freq.get(pair, 0) + 1
+            affected_pair_types.add(pair)
+             
+    if best_pair in pair_freq:
+        del pair_freq[best_pair]
+    if best_pair in pair_pos:
+        del pair_pos[best_pair]
+    if best_pair in affected_pair_types:
+        affected_pair_types.remove(best_pair)
 
-
+    for pair in affected_pair_types:
+        if pair in pair_freq:
+            heapq.heappush(pq, HeapItem(pair_freq[pair], pair))
 
 def train_bbpe(
     input_path: str | os.PathLike,
@@ -194,78 +189,41 @@ def train_bbpe(
     num_chunks: int = 4,
     **kwargs,
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-    """Given the path to an input corpus, run train a BPE tokenizer and
-    output its vocabulary and merges.
-
-    Args:
-        input_path (str | os.PathLike): Path to BPE tokenizer training data.
-        vocab_size (int): Total number of items in the tokenizer's vocabulary (including special tokens).
-        special_tokens (list[str]): A list of string special tokens to be added to the tokenizer vocabulary.
-            These strings will never be split into multiple tokens, and will always be
-            kept as a single token. If these special tokens occur in the `input_path`,
-            they are treated as any other string.
-
-    Returns:
-        tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-            vocab:
-                The trained tokenizer vocabulary, a mapping from int (token ID in the vocabulary)
-                to bytes (token bytes)
-            merges:
-                BPE merges. Each list item is a tuple of bytes (<token1>, <token2>),
-                representing that <token1> was merged with <token2>.
-                Merges are ordered by order of creation.
-    """
-    special_tokens_set = set(special_tokens)
-
-    # single process
-    # with open(input_path, 'rb') as f:
-    #     boundaries = find_chunk_boundaries(f, num_chunks, "<|endoftext|>".encode("utf-8"))
-
-    #     for start, end in zip(boundaries[:-1], boundaries[1:]):
-    #         f.seek(start)
-    #         chunk = f.read(end - start).decode("utf-8", errors="ignore")
-
     with open(input_path, 'rb') as f:
         boundaries = find_chunk_boundaries(f, num_chunks, "<|endoftext|>".encode("utf-8"))
+        
+        chunk_args = []
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk_bytes = f.read(end - start)
+            chunk_args.append((chunk_bytes.decode("utf-8", errors="ignore"), special_tokens))
 
-        # print(f"boundaries=({boundaries})")
+    with Pool(processes=min(cpu_count(), num_chunks)) as pool:
+        start_time = time.time()
+        results = list(pool.starmap(pre_tokenize, chunk_args))
+        end_time = time.time()
+        print(f"Pre-tokenization time: {end_time - start_time} seconds")
 
-        with Pool(processes=min(cpu_count(), num_chunks)) as pool:
-            start_time = time.time()
-            chunk_args = []
-            for start, end in zip(boundaries[:-1], boundaries[1:]):
-                f.seek(start)
-                chunk = f.read(end-start).decode("utf-8", errors="ignore")
-                chunk_args.append((chunk, special_tokens))
+    all_tokens = []
+    if not results:
+        return {}, []
 
-            results = list(pool.starmap(pre_tokenize, chunk_args))
+    vocab = results[0][1]
+    
+    for tokens, _ in results:
+        all_tokens.extend(tokens)
+    
+    merges = []
 
-            all_tokens = []
-
-            for tokens in results:
-                all_tokens.extend(tokens)
-
-            end_time = time.time()
-            print(f"Pre-tokenization time: {end_time - start_time} seconds")
-
-    # print(f"Total number of tokens: {len(all_tokens)}")
-    # print(f"Sample of tokens: {all_tokens[:5]}")
-
-    # optimized merging step
-    vocab = {} # token_id -> bytes
-    merges = [] # merge_rules
-    pair_pos = {} # pair -> list[tuple[int, int]]
-    for i in range(256):
-        vocab[len(vocab)] = bytes([i])
-
-    for i, token in enumerate(special_tokens):
-        vocab[len(vocab)] = token.encode('utf-8')
-
-
+    byte_to_token_id = {v: k for k, v in vocab.items()}
+    special_token_ids = set()
+    for st in special_tokens:
+        st_bytes = st.encode('utf-8')
+        if st_bytes in byte_to_token_id:
+            special_token_ids.add(byte_to_token_id[st_bytes])
 
     print(f"Initial vocab size: {len(vocab)}")
     print(f"Target vocab size: {vocab_size}")
-    # print(f"vocab: {vocab}")
     print(f"Progress bar total: {vocab_size - len(vocab)}")
 
     pbar = tqdm(
@@ -274,47 +232,45 @@ def train_bbpe(
     )
 
     start_time = time.time()
-
-    pair_freq, pair_pos = cal_freq(all_tokens, special_tokens)
+    pair_freq, pair_pos = cal_freq(all_tokens, special_token_ids)
     end_time = time.time()
     print(f"Frequency calculation time: {end_time - start_time} seconds")
 
-    # print(f"sample pair_freq: {list(pair_freq.items())[:5]}")
-    # print(f"sample all_tokens: {all_tokens[:5]}")
-    # print(f"sample max pair: {max(pair_freq.items(), key=lambda x: x[1])}")
+    HeapItem.vocab = vocab
 
-    top_12 = sorted(pair_freq.items(), key=lambda x: x[1], reverse=True)[:12]
-    print(f"top_12: {top_12}")
+    pq = []
+    for pair, freq in pair_freq.items():
+        heapq.heappush(pq, HeapItem(freq, pair))
 
     start_time = time.time()
-    step = 0
-    while len(vocab) < vocab_size:
-        
-        if len(pair_freq) == 0:
-            break
-        max_freq = max(pair_freq.values())
-        # Get all pairs with max_freq
-        candidates = [pair for pair, freq in pair_freq.items() if freq == max_freq]
-        # if step > 25 and step < 32:
-        #     for candidate in candidates:
-        #         print(f"candidate: {candidate}, freq: {pair_freq[candidate]}")
 
-        # Compare first elements, if equal then compare second elements
-        pair = max(candidates, key=lambda x: (x[0], x[1]))  # lexicographically greatest
-        if pair[0] in special_tokens_set or pair[1] in special_tokens_set:
-            continue
-        new_pair = pair[0] + pair[1]
-        # print(f"merge pair: {pair}")
-        all_tokens = merge_pair(pair, all_tokens, pair_freq, pair_pos)
-        merges.append(pair)
-        vocab[len(vocab)] = new_pair
-        step += 1
+    while len(vocab) < vocab_size:
+        if not pq:
+            break
+        top_item = None
+        while pq:
+            candidate = heapq.heappop(pq)
+            if candidate.pair in pair_freq and pair_freq[candidate.pair] == candidate.freq:
+                top_item = candidate
+                break
+        if top_item is None:
+            break
+
+        pair = top_item.pair
+        
+        new_token_id = len(vocab)
+        
+        merged_token_bytes = vocab[pair[0]] + vocab[pair[1]]
+        merges.append((vocab[pair[0]], vocab[pair[1]]))
+        vocab[new_token_id] = merged_token_bytes
+        
+        merge_pair(pair, all_tokens, pair_freq, pair_pos, pq, new_token_id)
+
         pbar.update(1)
+
     end_time = time.time()
     print(f"Merge time: {end_time - start_time} seconds")
     
     pbar.close()
-    # print(f"vocab: {vocab}")
-    # print(f"merges: {merges}")
     return vocab, merges
     
